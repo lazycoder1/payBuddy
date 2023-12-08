661be9a30228abe3af2e94130ff81bf76fb22fff
lazycoder1 <gautamgsabhahit@gmail.com>
2023-12-08 23:08:55 +0530

    adding user context acc. to safe repo

---
+++ frontend/src/contexts/userContext.tsx
@@ -0,0 +1,150 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+import { SafeAuthPack, SafeAuthConfig, SafeAuthInitOptions, SafeAuthUserInfo } from "@safe-global/auth-kit";
+import { sign } from "crypto";
+import { Button } from "flowbite-react";
+import { useEffect } from "react";
+
+import { createPublicClient, http, custom, createWalletClient, formatEther, hexToBigInt } from "viem";
+import { sepolia } from "viem/chains";
+
+const RPC_URL = "https://rpc.sepolia.org";
+
+const UserSessionContext = createContext({} as UserSession);
+
+export interface UserSession {
+    login: () => void;
+    logout: () => void;
+    userInfo: SafeAuthUserInfo | null;
+    chainId: string | null;
+    balance: bigint | null;
+    isAuthenticated: boolean;
+    safeAuthPack: SafeAuthPack | null;
+    eoa: string | null;
+}
+
+// Making the function which will wrap the whole app using Context Provider
+export default function UserSessionStore({ children }: any) {
+    const [safeAuthPack, setSafeAuthPack] = useState<SafeAuthPack | null>(null);
+    const [safeAuthSignInResponse, setSafeAuthSignInResponse] = useState<any>(null);
+    const [eoa, setEoa] = useState<string | null>(null);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [userInfo, setUserInfo] = useState<SafeAuthUserInfo | null>(null);
+    const [chainId, setChainId] = useState<string | null>(null);
+    const [balance, setBalance] = useState<bigint | null>(null);
+    const [walletClient, setWalletClient] = useState<any>(null);
+    const [publicClient, setPublicClinet] = useState<any>(null);
+
+    useEffect(() => {
+        // console.log("safeAuthSignInResponse", safeAuthSignInResponse);
+        setEoa(safeAuthSignInResponse?.eoa);
+    }, [safeAuthSignInResponse]);
+
+    useEffect(() => {
+        // @ts-expect-error - Missing globals
+        const params = new URL(window.document.location).searchParams;
+        const chainId = params.get("chainId");
+
+        const safeAuthConfig: SafeAuthConfig = {
+            txServiceUrl: "https://safe-transaction-base-goerli.safe.global",
+        };
+
+        (async () => {
+            const options: SafeAuthInitOptions = {
+                enableLogging: true,
+                buildEnv: "production",
+                chainConfig: {
+                    chainId: chainId || "0xaa36a7",
+                    rpcTarget: RPC_URL,
+                },
+            };
+
+            const authPack = new SafeAuthPack(safeAuthConfig);
+
+            await authPack.init(options);
+
+            // console.log("safeAuthPack:safeEmbed", authPack.safeAuthEmbed);
+
+            setSafeAuthPack(authPack);
+
+            authPack.subscribe("accountsChanged", async (accounts) => {
+                // console.log("safeAuthPack:accountsChanged", accounts, authPack.isAuthenticated);
+                if (authPack.isAuthenticated) {
+                    const signInInfo = await authPack?.signIn();
+
+                    setSafeAuthSignInResponse(signInInfo);
+                    setIsAuthenticated(true);
+                }
+            });
+
+            authPack.subscribe("chainChanged", (eventData) => console.log("safeAuthPack:chainChanged", eventData));
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (!safeAuthPack || !isAuthenticated) return;
+        (async () => {
+            const web3Provider = safeAuthPack.getProvider();
+            const userInfo = await safeAuthPack.getUserInfo();
+
+            setUserInfo(userInfo);
+
+            if (web3Provider) {
+                const [account] = await web3Provider.request({ method: "eth_requestAccounts" });
+
+                const publicClient = createPublicClient({
+                    chain: sepolia,
+                    transport: http(RPC_URL),
+                });
+
+                const walletClient = createWalletClient({
+                    account,
+                    chain: sepolia,
+                    transport: custom(web3Provider),
+                });
+
+                setChainId((await walletClient.getChainId()).toString());
+                const addresses = await walletClient.getAddresses(); // Await getAddresses() function
+                const balance = await publicClient.getBalance({
+                    address: addresses[0],
+                });
+
+                setBalance(balance);
+                setWalletClient(walletClient);
+                setPublicClinet(publicClient);
+            }
+        })();
+    }, [isAuthenticated]);
+
+    const login = async () => {
+        const signInInfo = await safeAuthPack?.signIn();
+
+        setSafeAuthSignInResponse(signInInfo);
+
+        console.log(signInInfo);
+
+        setIsAuthenticated(true);
+    };
+
+    const logout = async () => {
+        await safeAuthPack?.signOut();
+
+        setSafeAuthSignInResponse(null);
+    };
+
+    const getUserInfo = async () => {
+        const userInfo = await safeAuthPack?.getUserInfo();
+    };
+
+    return (
+        <UserSessionContext.Provider value={{ login, logout, isAuthenticated, userInfo, chainId, balance, eoa, safeAuthPack }}>
+            {children}
+        </UserSessionContext.Provider>
+    );
+}
+
+// Make useUserSession Hook to easily use our context throughout the application
+export function useUserSession() {
+    return useContext(UserSessionContext);
+}
