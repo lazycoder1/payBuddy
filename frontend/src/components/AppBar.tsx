44412ccf3b90ae596cea8a91d6ff123134f2bf7a
lazycoder1 <gautamgsabhahit@gmail.com>
2023-12-08 23:18:13 +0530

    .

---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,34 @@
+import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { SafeAuthUserInfo } from "@safe-global/auth-kit";
+
+type AppBarProps = {
+    isLoggedIn: boolean;
+    onLogin: () => void;
+    onLogout: () => void;
+    userInfo?: SafeAuthUserInfo;
+};
+
+const AppBar = ({ isLoggedIn, onLogin, onLogout, userInfo }: AppBarProps) => {
+    return (
+        <Navbar fluid rounded>
+            <NavbarBrand href="https://flowbite-react.com">
+                <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Pay Buddy</span>
+            </NavbarBrand>
+            {isLoggedIn ? (
+                <div className="flex md:order-2">
+                    <Button onClick={onLogout}>Log Out</Button>
+                </div>
+            ) : (
+                <Button onClick={onLogin}>Login</Button>
+            )}
+            <NavbarCollapse>
+                <NavbarLink href="#" active>
+                    Home
+                </NavbarLink>
+            </NavbarCollapse>
+        </Navbar>
+    );
+};
+
+export default AppBar;
